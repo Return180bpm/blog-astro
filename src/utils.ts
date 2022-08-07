949e18be34cc5b2a9375b9f2a5d1d8606fd0c10a
Tom Szwaja <th.szwaja@gmail.com>
2022-08-07 16:16:04 +0200

    feat: create utils.ts

---
+++ src/utils.ts
@@ -0,0 +1,45 @@
+// Standard long form date
+// in: 'dd.mm.yyyy'
+// out: Saturday, August 6, 2022 < -- too many commas
+// const formatDate = (dateFromMarkdown: string): string => {
+// 	const [day_, month_, year_] = dateFromMarkdown.split('.')
+// 	const dateObj = new Date(Number(year_), Number(month_) - 1, Number(day_))
+// 	const options: Intl.DateTimeFormatOptions = {
+// 		weekday: 'long',
+// 		year: 'numeric',
+// 		month: 'long',
+// 		day: 'numeric'
+// 	}
+// 	const dateTimeFormat = new Intl.DateTimeFormat('en-US', options)
+
+// 	return `${dateTimeFormat.format(dateObj)}`
+// }
+
+// My custom date
+
+const pr = new Intl.PluralRules('en-US', { type: 'ordinal' })
+const suffixes = new Map([
+	['one', 'st'],
+	['two', 'nd'],
+	['few', 'rd'],
+	['other', 'th']
+])
+const formatOrdinal = (n) => {
+	const rule = pr.select(n)
+	const suffix = suffixes.get(rule)
+	return `${n}${suffix}`
+}
+// in: 'dd.mm.yyyy'
+// out: Saturday, August 6th 2022
+export const formatDateToCustom = (dateFromMarkdown: string): string => {
+	const [day, month, year] = dateFromMarkdown.split('.')
+	const dateObj = new Date(Number(year), Number(month) - 1, Number(day))
+	const monthName = new Intl.DateTimeFormat('en-US', {
+		month: 'long'
+	}).format(dateObj)
+	const weekday = new Intl.DateTimeFormat('en-US', {
+		weekday: 'long'
+	}).format(dateObj)
+
+	return `${weekday}, ${monthName} ${formatOrdinal(day)} ${year}`
+}
