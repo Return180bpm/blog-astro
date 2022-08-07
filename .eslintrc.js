b96ab272aa9a186cec83168f2a44198793b60b51
Tom Szwaja <th.szwaja@gmail.com>
2022-08-07 12:42:58 +0200

    feat: add no-multi-spaces rule; format w\ prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,33 +2,34 @@ module.exports = {
 	env: {
 		node: true,
 		es2022: true,
-		browser: true,
+		browser: true
 	},
-	plugins: ["jsx-a11y"],
+	plugins: ['jsx-a11y'],
 	extends: [
-		"eslint:recommended",
-		"plugin:astro/recommended",
-		"plugin:jsx-a11y/recommended",
+		'eslint:recommended',
+		'plugin:astro/recommended',
+		'plugin:jsx-a11y/recommended'
 	],
 	parserOptions: {
-		ecmaVersion: "latest",
-		sourceType: "module",
+		ecmaVersion: 'latest',
+		sourceType: 'module'
 	},
 	overrides: [
 		{
 			// Define the configuration for `.astro` file.
-			files: ["*.astro"],
+			files: ['*.astro'],
 			// Allows Astro components to be parsed.
-			parser: "astro-eslint-parser",
+			parser: 'astro-eslint-parser',
 			// Parse the script in `.astro` as TypeScript by adding the following configuration.
 			// It's the setting you need when using TypeScript.
 			parserOptions: {
-				parser: "@typescript-eslint/parser",
-				extraFileExtensions: [".astro"],
+				parser: '@typescript-eslint/parser',
+				extraFileExtensions: ['.astro']
 			},
 			rules: {
-				semi: ["error", "never"],
-			},
-		},
-	],
-};
+				semi: ['error', 'never'],
+				'no-multi-spaces': 'error'
+			}
+		}
+	]
+}
