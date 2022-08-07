12bb87bdeace2e3d8ef761e92618a8cb8a90121e
Tom Szwaja <th.szwaja@gmail.com>
2022-08-07 12:29:03 +0200

    feat(css): tweak theme and typography plugin
    * simiplify palette by only including used colors
    * add my own week 1 colors
    * attempt to customize typograpy plugin
    ---> works for link decoration but not general text-color
    ---> Moved customization to global.css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,50 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			amber: colors.amber,
+			stone: colors.stone,
+			dod: {
+				week1: {
+					burgundy: '#443038',
+					babypowder: '#FDFFFA',
+					red: '#FD7D94',
+					canary: '#FFFA98'
+				}
+			}
+		},
+		// extend: {
+		// 	typography: (theme) => ({
+		// 		DEFAULT: {
+		// 			css: {
+		// 				color: theme('colors.dod.week1.burgundy'),
+		// 				a: {
+		// 					textDecorationColor: theme('colors.dod.week1.red'),
+		// 					textDecorationThickness: '4px'
+		// 				}
+
+		// 				// ...
+		// 			}
+		// 		},
+		// 		xl: {
+		// 			css: {
+		// 				color: theme('colors.dod.week1.burgundy'),
+		// 				a: {
+		// 					textDecorationColor: theme('colors.dod.week1.red'),
+		// 					textDecorationThickness: '4px'
+		// 				}
+
+		// 				// ...
+		// 			}
+		// 		}
+		// 	})
+		// },
 		fontFamily: {
 			sans: ['Padauk', 'sans-serif'],
 			serif: ['Merriweather', 'serif']
