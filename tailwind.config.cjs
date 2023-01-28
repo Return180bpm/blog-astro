637445afbf7b657bf90d7f974b6bf136d2c30091
Tom Szwaja <th.szwaja@gmail.com>
2023-01-28 21:15:04 +0100

    chore: tidy up comments

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,42 +6,16 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		colors: {
+			dark: 'hsl(var(--dark) / <alpha-value>)',
+			light: 'hsl(var(--light) / <alpha-value>)',
 			transparent: 'transparent',
 			current: 'currentColor',
 			amber: colors.amber,
 			stone: colors.stone,
-			dark: 'hsl(var(--dark) / <alpha-value>)',
-			light: 'hsl(var(--light) / <alpha-value>)',
 			babypowder: '#FDFFFA',
 			red: '#FD7D94',
 			canary: '#FFFA98'
 		},
-		// extend: {
-		// 	typography: (theme) => ({
-		// 		DEFAULT: {
-		// 			css: {
-		// 				color: theme('colors.dod.week1.burgundy'),
-		// 				a: {
-		// 					textDecorationColor: theme('colors.dod.week1.red'),
-		// 					textDecorationThickness: '4px'
-		// 				}
-
-		// 				// ...
-		// 			}
-		// 		},
-		// 		xl: {
-		// 			css: {
-		// 				color: theme('colors.dod.week1.burgundy'),
-		// 				a: {
-		// 					textDecorationColor: theme('colors.dod.week1.red'),
-		// 					textDecorationThickness: '4px'
-		// 				}
-
-		// 				// ...
-		// 			}
-		// 		}
-		// 	})
-		// },
 		fontFamily: {
 			sans: ['Arial', 'sans-serif'],
 			chonk: ['"Arial Black"', 'sans-serif'],
