98fbdd1ed037fb0efafea40ae7e061008b1cba52
Tom Szwaja <th.szwaja@gmail.com>
2023-06-02 20:01:37 +0200

    chore: move unused utils

--- src/utils.ts
+++ src/utils.ts
@@ -44,6 +44,17 @@ export function sortPostsByDate(posts: CollectionEntry<"posts">[]) {
 }
 
 // ### UNUSED but might be useful ###
+
+// export function getRandomFromArray(list) {
+// 	return list[Math.floor(Math.random() * list.length)]
+// }
+
+// export function getRandomInt(min, max) {
+// 	min = Math.ceil(min)
+// 	max = Math.floor(max)
+// 	return Math.floor(Math.random() * (max - min) + min) // The maximum is exclusive and the minimum is inclusive
+// }
+
 // in: 'd.m.yyyy'
 // const makeDateObj = (myDate: string): Date => {
 // 	const [day, month, year] = myDate.split('.')
